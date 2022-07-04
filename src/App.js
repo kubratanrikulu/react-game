59cd889bde5ac43197e18eee43a876ad15433ad7
Kübra Tanrıkulu <ktanrikuluu@gmail.com>
2022-07-05 00:11:19 +0300

    create-game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+const iconList = ['', 'X', 'O']
 function App() {
+  const [card, setCard] = useState([])
+  const [finalArray, setFinalArray] = useState([ [1,2,3,4,5,6,7,8,9].map( () => ({icon : ''}) )])
+  console.log(finalArray)
+
+  const push = () => {
+    let newList = []
+    let iconNumber = 0
+    for (let i = 0; i < 18; i=i+2) {
+      newList.push({
+        icon: iconList[iconNumber],
+        isOpen: false,
+        status: false
+      })
+      if (i % 3 === 1) {
+        iconNumber++
+      }
+    }
+    setCard(newList)
+    console.log(newList)
+  }
+  useEffect(() => {
+    push()
+  }, [])
+  const openCard = id => {
+    const item = finalArray[id].icon === '' ? 'X' : finalArray[id].icon === 'X' ?  'O' : ''
+    setFinalArray(item)
+    console.log(finalArray)
+   }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='row align-items-center justify-content-center'>
+          <h1 className='title mt-5'>GAME TIME</h1>
+          <div className='game'>
+            {
+              card.map((Item, index) =>
+                <div key={index} className='box' onClick={() => openCard(index)}>
+                   {Item.isOpen ?? Item.icon}
+                </div>
+              )
+            }
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
